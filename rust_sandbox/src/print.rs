254add72376b8f920f3123d552664ad2cf4dcead
Quackers <robq71@gmail.com>
2022-08-13 20:57:43 +0100

    print and formatting

--- rust_sandbox/src/print.rs
+++ rust_sandbox/src/print.rs
@@ -11,5 +11,18 @@ pub fn run() {
     println!("{0} is from {1} & {0} likes to {2}", "Q", "Hams", "play T&P" );
 
     // name args
+    println!(
+    "{name} likes to play {activity}",
+    name="Q",
+    activity="T&P"
+    );
 
+    // placeholder traits
+    println!("Binary: {:b} Hex: {:x} Octal {:o}", 10, 10, 10);
+
+    // placeholder for debug trait - tuple
+    println!("{:?}", (12, true, "hello"));
+
+    // Basic math
+    println!("10 + 10 = {}", 10 + 10);
 }
---
+++ rust_sandbox/target/debug/deps/rust_sandbox-bb4c549831b88a35.d
@@ -0,0 +1,6 @@
+/home/robq/repos/rust/rust_sandbox/target/debug/deps/rust_sandbox-bb4c549831b88a35.rmeta: src/main.rs src/print.rs
+
+/home/robq/repos/rust/rust_sandbox/target/debug/deps/rust_sandbox-bb4c549831b88a35.d: src/main.rs src/print.rs
+
+src/main.rs:
+src/print.rs:
