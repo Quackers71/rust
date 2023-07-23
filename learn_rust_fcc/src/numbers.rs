16d786e008cd059c86ac50795e9fc9534ce6a1bb
Quackers <q@quackers.com>
2023-07-23 21:43:24 +0100

    pushed default code example to test

--- learn_rust_fcc/src/numbers.rs
+++ learn_rust_fcc/src/numbers.rs
@@ -1,13 +1,23 @@
 // Numbers
-// Modify `assert_eq!` to make it work
-pub fn number() {
-    let x: u32 = 5; // added : u32 
-    assert_eq!("u32".to_string(), type_of(&x));
 
-    println!("Success!");
-}
+// No. 5 - Fix errors and panics to make it work
+fn main() {
+    let v1 = 251_u8 + 8;
+    let v2 = i8::checked_add(251, 8).unwrap();
+    println!("{},{}",v1,v2);
+ }
+ 
 
-// Get the type of given variable, return a string representation of the type  , e.g "i8", "u8", "i32", "u32"
-fn type_of<T>(_: &T) -> String {
-    format!("{}", std::any::type_name::<T>())
-}
+
+// // No. 3 - Modify `assert_eq!` to make it work
+// pub fn number() {
+//     let x: u32 = 5; // added : u32 
+//     assert_eq!("u32".to_string(), type_of(&x));
+
+//     println!("Success!");
+// }
+
+// // Get the type of given variable, return a string representation of the type  , e.g "i8", "u8", "i32", "u32"
+// fn type_of<T>(_: &T) -> String {
+//     format!("{}", std::any::type_name::<T>())
+// }
