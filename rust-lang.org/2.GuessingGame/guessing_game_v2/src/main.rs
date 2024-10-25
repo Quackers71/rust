a9d6619fa5f77d88ef76c9e038149006c561ec88
Quackers <q@quackers.com>
2024-10-25 19:23:54 +0100

    revisited guessing game with version 2

---
+++ rust-lang.org/2.GuessingGame/guessing_game_v2/src/main.rs
@@ -0,0 +1,41 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+// The below code was taken from AI and works like the original guessing gane version ;-)
+
+fn main() {
+    println!("Welcome to the Guessing Game!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut guess_count = 0;
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        guess_count += 1;
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number)
+ {
+            std::cmp::Ordering::Less => println!("Too small!"),
+            std::cmp::Ordering::Greater => println!("Too big!"),
+            std::cmp::Ordering::Equal => {
+                println!("You   
+ win in {} guesses!", guess_count);
+                break;
+            }
+        }
+    }
+}
