0f0105f539dab862d0ef6d39a2f40218872aaa3c
Quackers <q@quackers.com>
2024-10-28 21:54:06 +0000

    added comments throughout the code for clarity

--- rust-lang.org/2.GuessingGame/guessing_game/src/main.rs
+++ rust-lang.org/2.GuessingGame/guessing_game/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     println!("Please guess the secret number, between 1 & 100!");
     println!("Enter 'q' to quit");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = rand::thread_rng().gen_range(1..=100); 
     // println!("The secret number is: {secret_number}");
     let mut count = 0u32;
 
@@ -15,21 +15,21 @@ fn main() {
         eprint!("Please input your guess : ");
         count +=1;
 
-        let mut guess = String::new();
+        let mut guess = String::new(); // creates a variable "guess" to store in the user input
 
-        io::stdin()
-            .read_line(&mut guess)
+        io::stdin() // calls the stdin function from the io module
+            .read_line(&mut guess) // calls the read_line method, passing &mut guess as the argument - &mut makes the reference mutable
             .expect("Failed to read line");
 
-        let guess: i32 = match guess.trim().parse() { //.expect("Please type a number!"); // Example of Shadowing
-            Ok(num) => {
+        let guess: i32 = match guess.trim().parse() { // Example of Shadowing (re-using guess) //.trim() removes whitespace // .parse converts guess into an i32
+            Ok(num) => { // enum variant Ok() - Successful Result
                 if num < 1 || num > 100 {
                     eprintln!("Please enter a number between 1 and 100.");
                     continue;
                 }
                 num
             },
-            Err(_) => {
+            Err(_) => { // enum variant Err{_} // _ catches all - If the parse is not able to convert the String into a Number
                 if guess.trim().to_lowercase() == "q" {
                     count -=1;
                     if count == 0 {
@@ -41,7 +41,7 @@ fn main() {
                         println!("You took {count} guesses");
                         println!("But decided to quit the game.");
                     }
-                    break;
+                    break; // breaks loop & ends program
                 } else {
                     eprintln!("Invalid Input, please type a number!");
                     continue;
@@ -50,8 +50,8 @@ fn main() {
         };
         println!("You guessed: {guess}");
 
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too low!"),
+        match guess.cmp(&secret_number) { // match expression with cmp call between guess & secret_number
+            Ordering::Less => println!("Too low!"), // Ordering type with enum variants (Less, Greater & Equal)
             Ordering::Greater => println!("Too high!"),
             Ordering::Equal => {
                 if count == 1 {
@@ -61,7 +61,7 @@ fn main() {
                     println!("You win! the number was {secret_number}");
                     println!("It took you {count} guesses");
                 }
-                break;
+                break; // breaks loop & ends program...
             }
         }
     }
