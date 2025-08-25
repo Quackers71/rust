4f45e45cbfdba57fbb763a518715c33a3ba2699e
Rob Q <robq@Robs-MacBook-Pro.local>
2025-08-25 19:46:05 +0100

    if else, for & while loops

--- rust_for_sol/hello/src/main.rs
+++ rust_for_sol/hello/src/main.rs
@@ -1,12 +1,33 @@
 // mod arrays;
 
 fn main() {
+
+    // if, else if, else
     let n = 3;
+    println!("The number is {}", n);
     if n > 0 {
-        println!("greater than 0");
+        println!("which is greater than 0");
     } else if n < 0 {
-        println!("less than 0");
+        println!("which is less than 0");
     } else {
-        println!("is 0");
+        println!("which is 0");
+    }
+
+    // for loop
+    println!("for loop 1..6");
+    for i in 1..6 {
+        println!("{},", i);
+    }
+
+    // while loop
+    println!("while loop - starts at 1 and < 6 with a break at 4");
+    let mut l = 1;
+    while l < 6 {
+        println!("{}", l);
+        l += 1;
+        if l == 4 {
+            println!("exit");
+            break
+        }
     }
 }
