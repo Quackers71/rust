55818b227f9c22f8a41c773c9164c9573936052b
Rob Q <robq@Robs-MacBook-Pro.local>
2025-08-25 19:23:35 +0100

    if, else if, else

---
+++ rust_for_sol/hello/src/archived_main.rs
@@ -0,0 +1,30 @@
+/* let a = 10;
+    let b = 15;
+    println!("Hello, Rust!, a= {} & b= {}", a, b); */
+
+    /* tuples::tuple();
+    
+    let num = 10;
+    println!("your number {} is {}", num, even::is_even(num)); */
+
+   //  muts::muta();
+
+   // arrays::borrowing_slice();
+
+/*    let arr = [0, 1, 2, 3]; // length
+   let slice = &arr[1 .. 3]; // [1, 2] don't know the length
+
+   arrays::borrowing_slice(arr, slice); */
+
+   let str: &str = "Hello Rust!";
+   let mut string: String = String::from("Hello Rust");
+
+   let slice = &string[.. 6];
+   slice.len();
+
+   string.push_str(" ");
+   string.push('1');
+   string.push_str("! Bob");
+   string = string.replace("Hello", "Bye");
+
+   println!("{}", string);
--- rust_for_sol/hello/src/main.rs
+++ rust_for_sol/hello/src/main.rs
@@ -1,37 +1,12 @@
-// mod tuples;
-// mod even;
-// mod muts;
 // mod arrays;
 
 fn main() {
-    /* let a = 10;
-    let b = 15;
-    println!("Hello, Rust!, a= {} & b= {}", a, b); */
-
-    /* tuples::tuple();
-    
-    let num = 10;
-    println!("your number {} is {}", num, even::is_even(num)); */
-
-   //  muts::muta();
-
-   // arrays::borrowing_slice();
-
-/*    let arr = [0, 1, 2, 3]; // length
-   let slice = &arr[1 .. 3]; // [1, 2] don't know the length
-
-   arrays::borrowing_slice(arr, slice); */
-
-    let str: &str = "Hello Rust!";
-    let mut string: String = String::from("Hello Rust");
-
-    let slice = &string[.. 6];
-    slice.len();
-
-    string.push_str(" ");
-    string.push('1');
-    string.push_str("! Bob");
-    string = string.replace("Hello", "Bye");
-
-    println!("{}", string);
+    let n = 3;
+    if n > 0 {
+        println!("greater than 0");
+    } else if n < 0 {
+        println!("less than 0");
+    } else {
+        println!("is 0");
+    }
 }
