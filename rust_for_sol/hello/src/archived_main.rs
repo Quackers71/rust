9dc9e568929d8a3463011655698c20f15f51ba0d
Rob Q <robq@Robs-MacBook-Pro.local>
2025-08-25 20:37:13 +0100

    structs (classes)

--- rust_for_sol/hello/src/archived_main.rs
+++ rust_for_sol/hello/src/archived_main.rs
@@ -59,4 +59,13 @@ fn main() {
            break
        }
    }
+
+   let i = 2;
+    println!("i = {}", i);
+    match i {
+        0 => println!("0"),
+        1 | 2 => println!("matched to either 1 or 2"),
+        3..=4 => println!("matched to either 3 or 4"),
+        _ => println!("default")
+    }
 }
--- rust_for_sol/hello/src/main.rs
+++ rust_for_sol/hello/src/main.rs
@@ -1,14 +1,21 @@
 // mod arrays;
 
 fn main() {
+    let name = String::from("Peregrine Falcon"); // define the name ("Raven") and type String
+    let bird =  Bird {name, attack: 10}; // create instance of your struct = Struct Type {define types for all fields}
+    bird.print_details(); // call the instance and method form the struct
+}
 
-    let i = 2;
-    println!("i = {}", i);
-    match i {
-        0 => println!("0"),
-        1 | 2 => println!("matched to either 1 or 2"),
-        3..=4 => println!("matched to either 3 or 4"),
-        _ => println!("default")
-    }
-    
+// struct similar to classes
+struct Bird {
+    name: String,
+    attack: u64
+}
+
+// to implement a method to your struct (class)
+impl Bird {
+    fn print_details(&self) {
+        println!("The type of bird is a {}", self.name);
+        println!("It's attack value is {}", self.attack);
+    } 
 }
