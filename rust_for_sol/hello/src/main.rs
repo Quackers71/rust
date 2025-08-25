6be51279940e64e6f11e83fce67594cb3db33acb
Rob Q <robq@Robs-MacBook-Pro.local>
2025-08-25 22:13:46 +0100

    traits (interfaces)

--- rust_for_sol/hello/src/main.rs
+++ rust_for_sol/hello/src/main.rs
@@ -4,6 +4,9 @@ fn main() {
     let name = String::from("Peregrine Falcon"); // define the name ("Peregrine Falcon") and type String
     let bird =  Bird {name, attack: 10}; // create instance of your struct = Struct Type {define types for all fields}
     bird.print_details(); // call the instance and method form the struct
+
+    println!("The bird can fly : {}", bird.can_fly());
+    println!("The bird is an animal : {}", bird.is_animal());
 }
 
 // struct similar to classes
@@ -18,4 +21,18 @@ impl Bird {
         println!("The type of bird is a {}", self.name);
         println!("It's attack value is {}", self.attack);
     } 
+}
+
+impl Animal for Bird {
+    fn can_fly(&self) -> bool {
+        true
+    }
+}
+
+// Rust does not support inheritance where you extend a class (OOP), it only supports interfaces (traits)
+trait Animal {
+    fn can_fly(&self) -> bool;
+    fn is_animal(&self) -> bool {
+        true
+    }
 }
