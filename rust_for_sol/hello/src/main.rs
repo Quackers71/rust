0a9fb9fd19f0b6c36ad6b7ccb76f58149ebe0e99
Rob Q <robq@Robs-MacBook-Pro.local>
2025-08-25 18:32:16 +0100

    strings, push & replace

--- rust_for_sol/hello/src/main.rs
+++ rust_for_sol/hello/src/main.rs
@@ -1,7 +1,7 @@
 // mod tuples;
 // mod even;
 // mod muts;
-mod arrays;
+// mod arrays;
 
 fn main() {
     /* let a = 10;
@@ -17,8 +17,21 @@ fn main() {
 
    // arrays::borrowing_slice();
 
-   let arr = [0, 1, 2, 3]; // length
+/*    let arr = [0, 1, 2, 3]; // length
    let slice = &arr[1 .. 3]; // [1, 2] don't know the length
 
-   arrays::borrowing_slice(arr, slice);
+   arrays::borrowing_slice(arr, slice); */
+
+    let str: &str = "Hello Rust!";
+    let mut string: String = String::from("Hello Rust");
+
+    let slice = &string[.. 6];
+    slice.len();
+
+    string.push_str(" ");
+    string.push('1');
+    string.push_str("! Bob");
+    string = string.replace("Hello", "Bye");
+
+    println!("{}", string);
 }
