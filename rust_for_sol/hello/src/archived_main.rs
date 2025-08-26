9719f5459476d58ab923a86fd4af85a9f64ff46d
Rob Q <robq@Robs-MacBook-Pro.local>
2025-08-26 22:04:04 +0100

    enums - object(value)

--- rust_for_sol/hello/src/archived_main.rs
+++ rust_for_sol/hello/src/archived_main.rs
@@ -69,3 +69,43 @@ fn main() {
         _ => println!("default")
     }
 }
+
+// main and functions from structs and traits
+// mod arrays;
+
+fn main() {
+    let name = String::from("Peregrine Falcon"); // define the name ("Peregrine Falcon") and type String
+    let bird =  Bird {name, attack: 10}; // create instance of your struct = Struct Type {define types for all fields}
+    bird.print_details(); // call the instance and method form the struct
+
+    println!("The bird can fly : {}", bird.can_fly());
+    println!("The bird is an animal : {}", bird.is_animal());
+}
+
+// struct similar to classes
+struct Bird {
+    name: String,
+    attack: u64
+}
+
+// to implement a method to your struct (class)
+impl Bird {
+    fn print_details(&self) {
+        println!("The type of bird is a {}", self.name);
+        println!("It's attack value is {}", self.attack);
+    } 
+}
+
+impl Animal for Bird {
+    fn can_fly(&self) -> bool {
+        true
+    }
+}
+
+// Rust does not support inheritance where you extend a class (OOP), it only supports interfaces (traits)
+trait Animal {
+    fn can_fly(&self) -> bool;
+    fn is_animal(&self) -> bool {
+        true
+    }
+}
--- rust_for_sol/hello/src/main.rs
+++ rust_for_sol/hello/src/main.rs
@@ -1,38 +1,26 @@
 // mod arrays;
 
 fn main() {
-    let name = String::from("Peregrine Falcon"); // define the name ("Peregrine Falcon") and type String
-    let bird =  Bird {name, attack: 10}; // create instance of your struct = Struct Type {define types for all fields}
-    bird.print_details(); // call the instance and method form the struct
+    let a: MyEnum = MyEnum::A;
+    let b: MyEnum = MyEnum::B(5); 
+    let c: MyEnum = MyEnum::C{x:10, y:20}; 
+    println!("{:?}", a);
+    println!("{:?}", b);
+    println!("{:?}", c);
+    
+    if let MyEnum::B(val) = b {
+        println!("{}", val);
+    }
 
-    println!("The bird can fly : {}", bird.can_fly());
-    println!("The bird is an animal : {}", bird.is_animal());
-}
-
-// struct similar to classes
-struct Bird {
-    name: String,
-    attack: u64
-}
-
-// to implement a method to your struct (class)
-impl Bird {
-    fn print_details(&self) {
-        println!("The type of bird is a {}", self.name);
-        println!("It's attack value is {}", self.attack);
-    } 
-}
-
-impl Animal for Bird {
-    fn can_fly(&self) -> bool {
-        true
+    if let MyEnum::C{x, y} = c {
+        println!("{} {}",x, y);
     }
 }
 
-// Rust does not support inheritance where you extend a class (OOP), it only supports interfaces (traits)
-trait Animal {
-    fn can_fly(&self) -> bool;
-    fn is_animal(&self) -> bool {
-        true
-    }
+// enums are objects that represent a certain value
+#[derive(Debug)]
+enum MyEnum {
+    A,
+    B(i32),
+    C {x: i32, y: i32}
 }
