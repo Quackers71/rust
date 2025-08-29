62c860d6c3432b2c8135d84c8d48c9c654af29f3
Rob Q <robq@Robs-MacBook-Pro.local>
2025-08-29 21:01:20 +0100

    Options

--- rust_for_sol/hello/src/archived_main.rs
+++ rust_for_sol/hello/src/archived_main.rs
@@ -135,6 +135,26 @@ fn main() {
     vec.remove(0);
     println!("{:?}", vec);
     
+    let mut map = HashMap::new();
+
+    map.insert(0, "Hi");
+    map.insert(1, "Hi2");
+    println!("{:?}", map);
+
+    match map.get(&0) {
+        Some(str1) => println!("{}", str1),
+        None => println!("Doesn't exist in map"),
+    }
+
+    match map.get(&2) {
+        Some(str) => println!("{}", str),
+        _ => println!("Doesn't exist in map"),
+    }
+
+    map.remove(&0);
+    map.insert(2, "Hi3");
+    map.insert(5, "Hello Rust!");
+    println!("{:?}", map);
 }
 
 #[derive(Debug)]
--- rust_for_sol/hello/src/main.rs
+++ rust_for_sol/hello/src/main.rs
@@ -1,26 +1,24 @@
-use std::collections::HashMap;
+// use std::collections::HashMap;
 
-// Hash Map - objects to store data in a key value pairing 
+// Options - Enum that has two types - None/Some
+// None - to indicate failure or lack of value, and
+// Some(value), a tuple struct that wraps a value with Type T.
+
+fn divide(dividend: i32, divisor: i32) -> Option<i32> {
+    if dividend % divisor != 0 {
+        None
+    } else {
+        Some(dividend / divisor)
+    }
+}
 
 fn main() {
-    let mut map = HashMap::new();
+    let divide1: Option<i32> = divide(4, 2);
+    let divide2: Option<i32> = divide(2, 3);
 
-    map.insert(0, "Hi");
-    map.insert(1, "Hi2");
-    println!("{:?}", map);
+    // Unwrapping a 'Some' variant will extract the value wrapped
+    println!("{:?} unwraps to {}", divide1, divide1.unwrap());
 
-    match map.get(&0) {
-        Some(str1) => println!("{}", str1),
-        None => println!("Doesn't exist in map"),
-    }
-
-    match map.get(&2) {
-        Some(str) => println!("{}", str),
-        _ => println!("Doesn't exist in map"),
-    }
-
-    map.remove(&0);
-    map.insert(2, "Hi3");
-    map.insert(5, "Hello Rust!");
-    println!("{:?}", map);
+    // Unwrapping a 'None' variant will 'panic!'
+    // println!("{:?} unwraps to {}", divide2, divide2.unwrap());
 }
