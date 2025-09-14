d75c84138db2cdc6ec7595f7af42022596448208
Rob Q <robq@Robs-MacBook-Pro.local>
2025-09-14 12:45:43 +0100

    anchor build finished successfuly for now

--- solana/project-2-voting/voting-dapp/anchor/programs/voting/src/lib.rs
+++ solana/project-2-voting/voting-dapp/anchor/programs/voting/src/lib.rs
@@ -7,5 +7,36 @@ declare_id!("JAVuBXeBZqXNtS73azhBDAoYaaAFfo4gWXoZe2e7Jf8H");
 #[program]
 pub mod voting {
     use super::*;
+
+    pub fn initialize_poll(ctx: Context<InitializePoll>, _poll_id: u64) -> Result<()> {
+        Ok(())
+    }
 }
 
+#[derive(Accounts)]
+#[instruction(poll_id: u64)]
+pub struct InitializePoll<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + Poll::INIT_SPACE,
+        seeds = [poll_id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub poll: Account<'info, Poll>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Poll {
+    pub poll_id: u64,
+    #[max_len(280)]
+    pub description: String,
+    pub poll_start: u64,
+    pub poll_end: u64,
+    pub candidate_amount: u64,
+}
