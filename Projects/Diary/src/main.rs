06f870695a9af31abb08f0ded0a45cb4b6cfdf56
Quackerz <robq71@gmail.com>
2025-12-19 23:35:56 +0000

    implemented delete functionality, needs working on

--- Projects/Diary/src/main.rs
+++ Projects/Diary/src/main.rs
@@ -65,7 +65,7 @@ fn add_an_entry() -> Result<()> {
 
     let mut user_input = String::new();
 
-    println!("");
+    println!("\n--- Add an Entry ---");
     print!("Please add your entry : ");
     io::stdout().flush().expect("Failed to flush stdout");
     io::stdin().read_line(&mut user_input)?;
@@ -84,7 +84,7 @@ fn add_an_entry() -> Result<()> {
         .create(true) // create file if it doesn't exist
         .open(JOURNAL_FILE)?;
 
-    writeln!(&mut file, "[{}] [{}] - {}", id, timestamp, entry_to_write)?;
+    writeln!(&mut file, "[{}] [{}] {}", id, timestamp, entry_to_write)?;
     println!("You successfully added entry no. {} to the {}", id, JOURNAL_FILE);
     Ok(())
 }
@@ -101,8 +101,40 @@ fn get_next_id() -> Result<usize> {
     Ok(count + 1)
 }
 
-fn delete_an_entry() {
-    println!("");
-    println!("You chose to delete an entry");
-    println!("");
+fn delete_an_entry() -> Result<()> {
+    let _ = list_entries();
+
+    println!("\n--- Delete an entry ---");
+    println!("Enter the ID of the entry to delete : ");
+    io::stdout().flush().expect("Failed to flush stdout");
+
+    let mut id_input = String::new();
+    io::stdin().read_line(&mut id_input)?;
+    let id_to_delete = format!("[{}]", id_input.trim()); // match ID input
+
+    let file = File::open(JOURNAL_FILE)?;
+    let reader = BufReader::new(file);
+    let mut kept_lines = Vec::new();
+    let mut found = false;
+
+    for line in reader.lines() {
+        let line = line?;
+        // skip the line that with the targeted ID
+        if line.starts_with(&id_to_delete) {
+            found = true;
+            continue;
+        }
+        kept_lines.push(line);
+    }
+    if !found {
+        println!("Entry with ID {} not found.", id_input.trim());
+        return Ok(());
+    }
+    // overwrite the file with filtered lines
+    let mut file = File::create(JOURNAL_FILE)?; // truncates the file automatically
+    for line in kept_lines {
+        writeln!(file, "{}", line)?;
+    }
+    println!("Entry #{} deleted successfully.", id_input.trim());
+    Ok(())
 }
