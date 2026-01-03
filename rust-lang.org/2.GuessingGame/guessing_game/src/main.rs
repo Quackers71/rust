6e749405d5ba2af1b3f7b7bdd2ee8ee11c0a6c5b
Quackerz <robq71@gmail.com>
2026-01-03 19:28:31 +0000

    added some color & fixed CR to added to count

--- rust-lang.org/2.GuessingGame/guessing_game/src/main.rs
+++ rust-lang.org/2.GuessingGame/guessing_game/src/main.rs
@@ -1,13 +1,14 @@
 use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
+use colored::*;
 
 fn main() {
     
-    println!("Please guess the secret number, between 1 & 100.");
-    println!("*** You have 10 attempts *** Enter 'q' to quit");
+    println!("{}","Please guess the secret number, between 1 & 100.".yellow());
+    println!("{}",format!("*** You have {MAX_ATTEMPTS} attempts *** Enter 'q' to quit").on_bright_blue());
 
-    let secret_number = rand::thread_rng().gen_range(1..=100); 
+    let secret_number = rand::rng().random_range(1..=100);
     // println!("The secret number is: {secret_number}");
     let mut count = 0u32;
     const MAX_ATTEMPTS: u32 = 10;
@@ -25,27 +26,34 @@ fn main() {
         let guess: i32 = match guess.trim().parse() { // Example of Shadowing (re-using guess) //.trim() removes whitespace // .parse converts guess into an i32
             Ok(num) => { // enum variant Ok() - Successful Result
                 if num < 1 || num > 100 {
-                    eprintln!("Please enter a number between 1 and 100.");
+                    eprintln!("{}","Please enter a number between 1 and 100.".blue());
+                    // check for game over b4 continuing
+                    if count >= MAX_ATTEMPTS { break; }
                     continue;
                 }
                 num
             },
             Err(_) => { // enum variant Err{_} // _ catches all - If the parse is not able to convert the String into a Number
                 if guess.trim().to_lowercase() == "q" {
-                    // count +=1;
+
                     if count == 1 {
-                        println!("You decided to quit the game.");
+                        println!("{}","You decided to quit the game.".blue());
                     } else if count == 2 {
                         println!("You took {} legitimate guess", count - 1);
-                        println!("But decided to quit the game.");
+                        println!("{}","But decided to quit the game.".blue());
                     } else {
                         println!("You took {} legitimate guesses", count - 1);
-                        println!("But decided to quit the game.");
+                        println!("{}","But decided to quit the game.".blue());
                     }
                     break; // breaks loop & ends program
                 } else {
-                    eprintln!("Invalid Input, please type a number!");
-                    // count +=1;
+                    eprintln!("{}","Invalid Input, please type a number!".on_red());
+
+                    if count >= MAX_ATTEMPTS {
+                        println!("{} {}", "GAME OVER: ".on_red().bold(),format!("You've used all {MAX_ATTEMPTS} attempts.").red());
+                        println!("{}",format!("The secret number was : {secret_number}").blue());
+                        break;
+                    }
                     continue;
                 }
             }
@@ -53,23 +61,23 @@ fn main() {
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) { // match expression with cmp call between guess and &secret_number (again a reference)
-            Ordering::Less => println!("Too low!"), // Ordering type with enum variants (Less, Greater & Equal)
-            Ordering::Greater => println!("Too high!"),
+            Ordering::Less => println!("{}", "Too low!".red()), // Ordering type with enum variants (Less, Greater & Equal)
+            Ordering::Greater => println!("{}", "Too high!".red()),
             Ordering::Equal => {
                 if count == 1 {
-                    println!("What! You got the secret number in {count} guess...");
-                    println!("Please go and buy a lottery ticket today!");
+                    println!("{}",format!("What! You got the secret number in {count} guess...").on_green());
+                    println!("{}","Please go and buy a lottery ticket today!".yellow());
                 } else {
-                    println!("You win! the number was {secret_number}");
-                    println!("It took you {count} legitimate guesses");
+                    println!("{} {}", "You win! the number was ".green().bold(),format!("{secret_number}").on_green());
+                    println!("{}",format!("It took you {count} legitimate guesses").blue());
                 }
                 break; // breaks loop & ends program...
             }
         }
 
         if count >= MAX_ATTEMPTS {
-            println!("GAME OVER: You've used all {MAX_ATTEMPTS} attempts.");
-            println!("The secret number was : {secret_number}");
+            println!("{} {}", "GAME OVER: ".on_red().bold(),format!("You've used all {MAX_ATTEMPTS} attempts.").red());
+            println!("{}",format!("The secret number was : {secret_number}").blue());
             break;
         }
     }
